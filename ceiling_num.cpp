c6bb953b10331d100f3450a6cc6e186d02d3fac5
Syed Uzair Gillani <134830647+uzair-77@users.noreply.github.com>
2024-08-25 17:28:02 +0500

    Adding cpp files

---
+++ ceiling_num.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float x;
+    cout<<"Enter the number in points"<<endl;
+    cin>>x;
+    float result = ceil(x);
+    cout<<"The ceiling value of "<<x<<" is  "<< result<<endl;
+
+    return 0;
+}
